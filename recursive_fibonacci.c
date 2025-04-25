07dbcb103ed0135a141a75f9c0bd18578b276685
wilyJ80 <abjurandam@gmail.com>
2025-04-25 21:20:12 +0000

    feat: finish recursive fibonacci

--- recursive_fibonacci.c
+++ recursive_fibonacci.c
@@ -1,11 +1,15 @@
 #include "recursive_fibonacci.h"
 
 const int recursiveFibonacci(int nthNumber, int *steps) {
+	(*steps)++;
 	if (nthNumber == 0) {
 		return 0;
+
+		(*steps)++;
 	} else if (nthNumber == 1) {
 		return 1;
 	}
+	(*steps)++;
 	return recursiveFibonacci(nthNumber - 1, steps) + recursiveFibonacci(nthNumber - 2, steps);
 }
 
@@ -14,14 +18,14 @@ void initResults(struct RecursiveFibonacci *self) {
 	for (int i = 0; i < 5; i++) {
 		self->results[i].inputNumber = inputNumber;
 		self->results[i].steps = 0;
-		inputNumber *= 2; 
+		inputNumber += 10; 
 	}
 }
 
 void calculateResults(struct RecursiveFibonacci *self) {
 	for (int i = 0; i < 5; i++) {
 		const int result = recursiveFibonacci(self->results[i].inputNumber, &self->results[i].steps);
-		self->results[i].steps = 1;
+		// self->results[i].steps = 1;
 	}
 }
 
